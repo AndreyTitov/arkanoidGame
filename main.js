0bf4a371a9b132015be724ec93a60fab5fff2505
AndreyTitov <andpyxa07@gmail.com>
2018-06-05 16:07:36 +0300

    Change color of the ball

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function keyUpHandler(e) {
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0,Math.PI*2);
-    ctx.fillStyle = "lightblue";
+    ctx.fillStyle = "green";
     ctx.fill();
     ctx.closePath;
 };
